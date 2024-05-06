d1452c9477cff1513d6210b62de56974ceaa0972
tm-fk <165568896+tm-fk@users.noreply.github.com>
2024-05-06 17:49:59 +0900

    ファイルの追加

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>kadai_007</title>
+</head>
+
+<body>
+  <P>
+    <?php
+    $personal_date = ['name' => 'onion','price' => '200','weight' => '160'];
+
+    print_r($personal_date);
+    
+    ?>
+  </p>
+</body>
+
+</html>
